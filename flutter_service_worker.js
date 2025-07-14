f6a2952efd80b16d13c4fe2e9273426a7bd180c8
TohhiDockson <tomo2127@gmail.com>
2025-07-15 00:48:15 +0900

    Update for print-friendly result screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "feaf539b230f502ee1808a6bd64df2e0
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ebf9b6a82b7082f23a30adafc8cb7043",
+"flutter_bootstrap.js": "50fba89d2d6c165f28a7add44e81df5a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
