88a1742551c2cd62c0ed412a54ce09711fee81e6
TohhiDockson <tomo2127@gmail.com>
2025-07-15 01:00:08 +0900

    fix print timing with postFrameCallback

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "feaf539b230f502ee1808a6bd64df2e0
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "7b709d48af9927a69b7a4a7373c6a750",
+"flutter_bootstrap.js": "0baf6d2a0840a8c000545b5dce64c95f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "bd1bb086505a3d4ef34f3ea481ec2e69",
 "/": "bd1bb086505a3d4ef34f3ea481ec2e69",
-"main.dart.js": "f976ed6f25b09e052ad6d397848cb336",
+"main.dart.js": "ca2706c0b94e1e9bb7791eb2d787ffcd",
 "manifest.json": "0bef970a9b3f41da91f5f226a2d44ee8",
 "version.json": "4ecfea515b0481cf113abfe42cedd8fd"};
 // The application shell files that are downloaded before a service worker can
